bb2ebaca14d1b2caf75c49e10b96e6cc605ddbd5
ndmanhSoict <manhsoict@gmail.com>
2025-10-03 15:49:27 +0700

    chore: config ESLint + Prettier + commitlint

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,11 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -14,10 +16,17 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      prettier, // tắt rule ESLint xung đột với Prettier
     ],
+    plugins: {
+      prettier: eslintPluginPrettier, // khai báo plugin prettier
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      'prettier/prettier': 'error', // bật rule: format sai => ESLint báo lỗi
+    },
   },
-])
+]);
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 function App() {
-
   return (
     <>
-       <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
