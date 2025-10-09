36340c8ea831a8771a7c07d72165d679c1d305a8
ndmanhSoict <manhsoict@gmail.com>
2025-10-09 14:18:50 +0700

    feat: add type Task

--- src/routes/test.tsx
+++ src/routes/test.tsx
@@ -1,10 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router';
-import TestComponent from '../TestComponent';
 
 export const Route = createFileRoute('/test')({
-  component: () => (
-    <>
-      <TestComponent />
-    </>
-  ),
+  component: () => <>{/* <TestComponent /> */}</>,
 });
---
+++ src/shared/type/TypeTask.tsx
@@ -0,0 +1,16 @@
+export type Task = {
+  id: string;
+  title: string;
+  description: string;
+  status:
+    | 'CREATED'
+    | 'TODO'
+    | 'IN_PROGRESS'
+    | 'IN_REVIEW'
+    | 'DEPLOY'
+    | 'IN_TESTING'
+    | 'VERIFY'
+    | 'DONE';
+  createdAt: Date;
+  updatedAt: Date;
+};
