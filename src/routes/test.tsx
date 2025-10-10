0eaecda55bfebc1439d072aae75a0eb7e78304c1
ndmanhSoict <manhsoict@gmail.com>
2025-10-10 10:42:06 +0700

    feat: add route todo.tsx

--- src/routes/test.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router';
-import ToDoPage from '../features/todo/page/ToDoPage';
-export const Route = createFileRoute('/test')({
-  component: () => (
-    <>
-      <ToDoPage />
-    </>
-  ),
-});
