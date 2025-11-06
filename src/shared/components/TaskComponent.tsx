6a986493eb8e83145c8664221f65cd7ce757163a
ndmanhSoict <manhsoict@gmail.com>
2025-11-06 16:46:57 +0700

    feat: floating task dayleft

--- src/shared/components/TaskComponent.tsx
+++ src/shared/components/TaskComponent.tsx
@@ -1,49 +1,49 @@
 import { useState } from 'react';
 import type { Task } from '@type/TypeTask';
-import ModalTaskDetail from './ModalTaskDetail';
+import ModalTaskDetail from '@shared/components/ModalTaskDetail';
 import { TaskContext } from '@shared/context/TaskContext';
 import { getDayLeft } from '@shared/utils/TimeUtils';
 import { queryClient } from '@main';
+import {
+  useHover,
+  useFloating,
+  useInteractions,
+  safePolygon,
+  shift,
+  flip,
+  offset,
+} from '@floating-ui/react';
+import { getTaskColor } from '@shared/utils/ColorTaskUtils';
 
 export default function TaskComponent({ task }: { task: Task }) {
   const [openModalDetail, setOpenModalDetail] = useState(false);
+  const [isOpenFloating, setIsOpenFloating] = useState(false);
+  const { refs, floatingStyles, context } = useFloating({
+    placement: 'right-end',
+    middleware: [flip(), shift(), offset(10)],
+    open: isOpenFloating,
+    onOpenChange: setIsOpenFloating,
+  });
+  const dayLeft = getDayLeft(task.endDate);
+
+  const hover = useHover(context, {
+    delay: { open: 0, close: 0 },
+    handleClose: safePolygon({
+      requireIntent: false,
+    }),
+  });
+
+  const { getReferenceProps, getFloatingProps } = useInteractions([hover]);
+
   function handleOpenCloseModal(): void {
     setOpenModalDetail(!openModalDetail);
   }
 
-  function getTaskColor(priority: string, endDateString: string): string {
-    const dayLeft = getDayLeft(endDateString);
-
-    let bgColr: string = '';
-
-    if (dayLeft < 0) {
-      bgColr = 'bg-[var(--task-overdue-bg)] opacity-70';
-    } else if (dayLeft < 3) {
-      //Còn lại 0 1 2
-      bgColr = 'bg-[var(--task-soon-bg)]';
-    } else if (dayLeft <= 5) {
-      //Còn lại 3 4 5
-      bgColr = 'bg-[var(--task-medium-bg)]';
-    } else if (dayLeft <= 10) {
-      //Còn lại 6 -> 10
-      bgColr = 'bg-[var(--task-safe-bg)]';
-    } else {
-      bgColr = 'bg-[var(--task-long-bg)]'; //Hơn 10
-    }
-
-    const borderLine: Record<string, string> = {
-      LOW: 'border-r-[var(--task-border-low)]',
-      MEDIUM: 'border-r-[var(--task-border-medium)]',
-      HIGH: 'border-r-[var(--task-border-high)]',
-      HIGHEST: 'border-r-[var(--task-border-highest)]',
-      URGENT: 'border-r-[var(--task-border-urgent)]',
-    };
-
-    return `${bgColr} border-r-8 ${borderLine[priority]}`;
-  }
   return (
     <TaskContext.Provider value={{ handleOpenCloseModal }}>
       <div
+        ref={refs.setReference}
+        {...getReferenceProps()}
         className={`border p-2 mb-4 rounded w-48 block mx-auto ${getTaskColor(task.priority, task.endDate)} hover:shadow-xl hover:scale-102 transition-all`}
         // className={`border p-2 mb-4 rounded  shadow w-48 block mx-auto`}
         draggable={true}
@@ -59,6 +59,29 @@ export default function TaskComponent({ task }: { task: Task }) {
           {task.name}
         </h2>
       </div>
+
+      {isOpenFloating && (
+        <div
+          ref={refs.setFloating}
+          style={floatingStyles}
+          {...getFloatingProps()}
+          className="z-50 bg-[var(--color-bg)] p-2 rounded shadow text-[var(--color-text)]"
+        >
+          {dayLeft === 0 ? (
+            <>
+              <strong>Today</strong> is the deadline for this task
+            </>
+          ) : dayLeft < 0 ? (
+            <>This task is overdue.</>
+          ) : (
+            <>
+              <strong>{dayLeft}</strong> {dayLeft === 1 ? 'day ' : 'days '}
+              left until this task's deadline
+            </>
+          )}
+        </div>
+      )}
+
       {openModalDetail && <ModalTaskDetail task={task}></ModalTaskDetail>}
     </TaskContext.Provider>
   );
